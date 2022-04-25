a223f52b80a31a6bb07bd11d93902ec5857d29a7
Narc2022 <102596511+Narc2022@users.noreply.github.com>
2022-04-25 21:29:52 +0530

    data type

---
+++ 02Datatype/index.js
@@ -0,0 +1,9 @@
+/*******Datatype in Js*******/
+
+let myName = "Sachin Singh";
+console.log(myName);
+console.log(typeof(myName));
+
+let myAge = 26;
+console.log(myAge);
+console.log(typeof(myAge));
