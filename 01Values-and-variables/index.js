10f85061fd8f2cda75558a6f85bd19536d426087
Narc2022 <102596511+Narc2022@users.noreply.github.com>
2022-04-25 19:44:49 +0530

    value variable all js interview questions

--- 01Values-and-variables/index.js
+++ 01Values-and-variables/index.js
@@ -1,4 +1,17 @@
 /**values and variable**/
-let myName = 'Sachin Singh';
-let myAge = '26';
-console.log(myName+" "+myAge);
+// let myName = 'Sachin Singh';
+// let myAge = '26';
+// console.log(myName+" "+myAge);
+
+
+// Naming Practice
+
+// let _myName = "Sachin Singh";
+//let 1myName = "Sachin Singh";
+let _1myName = "Sachin Singh";
+let $myName = "Sachin Singh";
+
+// console.log(_myName);
+// // console.log(1myName);
+console.log(_1myName);
+console.log($myName);
