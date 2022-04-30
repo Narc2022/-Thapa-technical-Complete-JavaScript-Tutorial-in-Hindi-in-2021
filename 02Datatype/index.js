a85893bab337158f7e4f79ec241395a2518bffa8
Narc2022 <102596511+Narc2022@users.noreply.github.com>
2022-04-30 13:08:34 +0530

    Interview questions

--- 02Datatype/index.js
+++ 02Datatype/index.js
@@ -1,9 +1,39 @@
 /*******Datatype in Js*******/
 
-let myName = "Sachin Singh";
-console.log(myName);
-console.log(typeof(myName));
+// let myName = "Sachin Singh";
+// console.log(myName);
+// console.log(typeof(myName));
 
-let myAge = 26;
-console.log(myAge);
-console.log(typeof(myAge));
+// let myAge = 26;
+// console.log(myAge);
+// console.log(typeof(myAge));
+
+
+/*******Datatype Practice*******/
+// 9-"5"
+// console.log(9 - "5"); //bug
+// // "Java" + "Script"
+// console.log("Java" + "Script");
+// // " " + " "
+// console.log(" " - " ");
+// // " " + 0
+// console.log(" " + 0);
+// // "SACHIN" - "SINGH"
+// console.log("SACHIN" - "SINGH");
+// // true + true
+// console.log(true + true);
+// // true + false
+// console.log(true + false);
+// // false + true
+// console.log(false + true);
+// // false - true
+// console.log(false - true);
+
+/********Interview Question
+Difference between null vs undefined**************/
+
+var iAmUseless = null;
+console.log(iAmUseless);
+console.log(typeof(iAmUseless));
+var iAmStandBy;
+console.log(iAmStandBy);
