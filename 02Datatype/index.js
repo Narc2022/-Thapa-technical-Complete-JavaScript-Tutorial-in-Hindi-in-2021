ed5380c8cb6f06e8f370fd94afe82b48974d3d90
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-04-30 23:02:14 +0530

    update

--- 02Datatype/index.js
+++ 02Datatype/index.js
@@ -29,11 +29,22 @@
 // // false - true
 // console.log(false - true);
 
-/********Interview Question
+/********Interview Question 1
 Difference between null vs undefined**************/
 
-var iAmUseless = null;
-console.log(iAmUseless);
-console.log(typeof(iAmUseless));
-var iAmStandBy;
-console.log(iAmStandBy);
+// var iAmUseless = null;
+// console.log(iAmUseless);
+// console.log(typeof(iAmUseless));
+// var iAmStandBy;
+// console.log(iAmStandBy);
+
+/********Interview Question 2
+6.What is NaN?
+=>initial value is Not a Number.
+=>NaN is the property of the global object.**************/
+
+// var myPhoneNumber = 9876543210;
+// var myName = "naRc tech";
+
+// console.log(isNaN(myPhoneNumber));
+// console.log(isNaN(myName));
