17c0701db76b1bf0f3bbb00adb6f8c483088984a
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-05-31 20:17:17 +0530

    Update index.js

--- 05ECMAScript 2015 (ES6) Part 1/Array/index.js
+++ 05ECMAScript 2015 (ES6) Part 1/Array/index.js
@@ -15,7 +15,7 @@ var myfriends = ['raj',22,'MALE','arjun',20,'MALE','krishna','LOVE',5000];*/
 /************************7.2 Treversal of Array*************/
 
 
-var myfriends = ['raj','arjun','krishna'];
+var myfriends = ['raj','arjun','krishna','vibha','rutuja'];
 // console.log(myfriends[0]);
 for(i=0;i<=myfriends.length-1;i++){
     console.log(myfriends[i]);
